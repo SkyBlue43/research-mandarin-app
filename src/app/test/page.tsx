7f313e07c0692669634cc4cfae0e8d96dc845565
Skyler <SkyBlue43@users.noreply.github.com>
2025-07-14 10:39:55 -0600

    Timer and phrase in heading

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,12 +1,31 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
-import { useEffect } from 'react'
+import { useSearchParams, useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 export default function TestPage() {
+  const [characters, setCharacters] = useState<any>([])
   const searchParams = useSearchParams();
   const test = searchParams.get('test');
   const group = searchParams.get('group')
+  const router = useRouter();
+  const [timeLeft, setTimeLeft] = useState(900); // seconds
+  const [arrayIndex, setArrayIndex] = useState(0);
+  const [currentPhrase, setCurrentPhrase] = useState('');
+  const [currentPinyin, setCurrentPinyin] = useState('');
+
+  useEffect(() => {
+    if (timeLeft === 0) {
+      router.push('/test_over');
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      setTimeLeft((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [timeLeft]);
 
   useEffect(() => {
     const fetchCharacters = async () => {
@@ -21,14 +40,31 @@ export default function TestPage() {
       });
       const data = await result.json();
       console.log(data);
+      setCharacters(data.characters);
+      setCurrentPhrase(data.characters[arrayIndex]['chinese']);
+      setCurrentPinyin(data.characters[arrayIndex]['pinyin'])
     };
     fetchCharacters();
   }, [test]);
 
   return (
-    <div className="p-8">
-      <h1 className="text-xl font-bold">You selected test: {test}</h1>
-      <h1 className="text-xl font-bold">Group: {group}</h1>
+    <div className='h-screen flex flex-col items-center'>
+      <header className='m-4'>
+        <div className='text-3xl font-bold bg-blue-500 p-3 rounded-xl border border-[#ffffff]'>
+          Time Left: {timeLeft}
+        </div>
+        <div>
+          {currentPhrase}
+        </div>
+        <div>
+          {currentPinyin}
+        </div>
+      </header>
+
+      <div className="p-8">
+        <h1 className="text-xl font-bold">You selected test: {test}</h1>
+        <h1 className="text-xl font-bold">Group: {group}</h1>
+      </div>
     </div>
   );
 }
