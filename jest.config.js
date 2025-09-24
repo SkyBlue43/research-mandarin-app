7181576d36ab996e2846da9e41162dde6554ce54
Skyler <SkyBlue43@users.noreply.github.com>
2025-09-24 11:49:04 -0600

    Aded Jest and workflows to the project

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('jest').Config} */
+module.exports = {
+    testEnvironment: "jsdom",
+    transform: {
+      "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "tsconfig.json" }],
+    },
+    moduleNameMapper: {
+      "^@/(.*)$": "<rootDir>/src/$1",
+    },
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  };
+  
