35874cd081721fe605740b577765d9d2c5394858
Skyler <SkyBlue43@users.noreply.github.com>
2025-10-20 14:44:05 -0600

    Adds

---
+++ .next/static/chunks/src_app_practice_session_page_tsx_8f972fda._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_4bbf9b72._.js",
+  "static/chunks/395b4_recharts_es6_a333c1eb._.js",
+  "static/chunks/48a02_sweetalert2_dist_sweetalert2_all_2b39feb7.js",
+  "static/chunks/node_modules__pnpm_e49d44ad._.js"
+],
+    source: "dynamic"
+});
