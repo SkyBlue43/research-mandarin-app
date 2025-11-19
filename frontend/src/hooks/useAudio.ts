8bb212b30047d55ee562f487a71a4bd3a46b29b6
Skyler <SkyBlue43@users.noreply.github.com>
2025-11-19 16:36:41 -0700

    Fixed a hard coded link

--- frontend/src/hooks/useAudio.ts
+++ frontend/src/hooks/useAudio.ts
@@ -1,11 +1,13 @@
-import {useState, useEffect} from 'react'
+const BASE = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+import { useState, useEffect } from "react";
 
 export function useAudio(test: string | null, currentIndex: string) {
-    const [chosenAudio, setChosenAudio] = useState('');
+  const [chosenAudio, setChosenAudio] = useState("");
 
-    useEffect(() => {
-        setChosenAudio(`http://localhost:8000/sounds/${test}/${currentIndex}.mp3`);
-    }, [currentIndex]);
+  useEffect(() => {
+    setChosenAudio(`${BASE}/sounds/${test}/${currentIndex}.mp3`);
+  }, [currentIndex]);
 
-    return { chosenAudio }
+  return { chosenAudio };
 }
