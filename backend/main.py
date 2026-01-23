9072986a7f75191d28825aa3737486e5909d7975
Skyler <SkyBlue43@users.noreply.github.com>
2026-01-23 11:20:19 -0700

    Updated CORS

--- backend/main.py
+++ backend/main.py
@@ -39,13 +39,18 @@ app = FastAPI()
 #     allow_headers=["*"],
 # )
 
+origins = [
+    "http://localhost:3000",      # local frontend testing
+    "http://127.0.0.1:8080",
+    "https://tones.cs.byu.edu",   # your eventual server URL
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://research-mandarin-app.vercel.app",
-         "https://*.vercel.app", "http://localhost:3000"],  # local or production URL
-    allow_credentials=True,        # only if you use cookies or auth headers
-    allow_methods=["*"],           # GET, POST, PUT, DELETE, OPTIONS, etc.
-    allow_headers=["*"],           # all headers
+    allow_origins=origins,       # which domains can access your backend
+    allow_credentials=True,      # allow cookies/credentials
+    allow_methods=["*"],         # GET, POST, PUT, DELETE…
+    allow_headers=["*"],         # headers like Content-Type
 )
 
 @app.get("/")
