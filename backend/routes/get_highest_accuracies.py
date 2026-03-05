bda60893b3a03ccf7f00ba708dd6e96e262856fe
Skyler <SkyBlue43@users.noreply.github.com>
2026-03-05 11:57:15 -0700

    Added the last accuracy part to the DB

--- backend/routes/get_highest_accuracies.py
+++ backend/routes/get_highest_accuracies.py
@@ -1,40 +1,28 @@
-import os
-import csv
-
-def read_lines(infile):
-    with open(infile, newline='', encoding='utf-8') as file:
-        reader = csv.reader(file)
-        return list(reader)
+from database import get_connection
 
 async def get_highest_accuracies_for_user(name, test, group):
+    conn = get_connection()
+    cur = conn.cursor()
 
-    try: 
-        base_dir = os.path.join(os.getcwd(), "data")
-        base_dir = os.path.join(base_dir, f'Test_{test}')
-        filename = os.path.join(base_dir, f"{name}_{group}.csv")
+    cur.execute("""
+                SELECT character_id, MAX(accuracy)
+                FROM attempts
+                WHERE user_id = %s
+                AND test = %s
+                GROUP BY character_id;
+                """, (name, test))
+    
+    rows = cur.fetchall()
 
-        lines = read_lines(filename)
-        accuracies = []
-        for items in lines:
-            if len(items) <= 2:
-                raise IndexError("Malformed line: not enough columns")
+    accuracies = []
 
-            if items[0] == 'Chinese':
-                continue
-            if len(items) > 3:
-                highest = 0
-                for i in range(2, len(items)):
-                    if float(items[i]) > highest:
-                        highest = float(items[i])
-            else:
-                highest = float(items[2])
-            accuracies.append({
-                'chinese': items[0],
-                'accuracy': highest
-            })
-        
-        return {'accuracies': accuracies}
-    except FileNotFoundError:
-        raise FileNotFoundError("File not found")
-    except PermissionError:
-        raise PermissionError("Permission denied reading file")
+    for chinese, max_accuracy in rows:
+        accuracies.append({
+            "chinese": chinese,
+            "accuracy": float(max_accuracy)
+        })
+
+    cur.close()
+    conn.close()
+
+    return {"accuracies": accuracies}
