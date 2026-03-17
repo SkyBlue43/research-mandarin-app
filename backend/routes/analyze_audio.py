634a512def6fe00a1e486e8c6f1361a070c3fdde
Skyler <SkyBlue43@users.noreply.github.com>
2026-03-17 12:03:15 -0600

    Changed to a tempfile

--- backend/routes/analyze_audio.py
+++ backend/routes/analyze_audio.py
@@ -1,6 +1,7 @@
 import parselmouth
 import numpy as np
 import shutil, os
+import tempfile
 from pydub import AudioSegment
 
 
@@ -25,60 +26,66 @@ def analyze_given_audio(infile):
     if infile is None or infile.file is None:
         raise ValueError("No audio file provided.")
 
-    input_path = "temp_input"
-    output_path = "temp.wav"
+    input_tmp = tempfile.NamedTemporaryFile(delete=False, prefix="audio_", suffix=".in")
+    input_path = input_tmp.name
+    input_tmp.close()
+    output_path = tempfile.NamedTemporaryFile(delete=False, prefix="audio_", suffix=".wav").name
 
-    with open(input_path, "wb") as buffer:
-        shutil.copyfileobj(infile.file, buffer)
+    try:
+        with open(input_path, "wb") as buffer:
+            shutil.copyfileobj(infile.file, buffer)
 
-    audio = AudioSegment.from_file(input_path)
+        audio = AudioSegment.from_file(input_path)
 
-    #clean audio
-    audio = audio.high_pass_filter(80)
-    audio = audio.low_pass_filter(8000)
+        #clean audio
+        audio = audio.high_pass_filter(80)
+        audio = audio.low_pass_filter(8000)
 
-    audio.export(output_path, format="wav")
+        audio.export(output_path, format="wav")
 
-    sound = parselmouth.Sound(output_path)
-    pitch = sound.to_pitch()
+        sound = parselmouth.Sound(output_path)
+        pitch = sound.to_pitch()
 
-    pitch_values = []
-    min_freq = float('inf')
-    max_freq = 0
-    for i in range(pitch.get_number_of_frames()):
-        time = pitch.get_time_from_frame_number(i + 1)
-        freq = pitch.get_value_in_frame(i + 1)
+        pitch_values = []
+        min_freq = float('inf')
+        max_freq = 0
+        for i in range(pitch.get_number_of_frames()):
+            time = pitch.get_time_from_frame_number(i + 1)
+            freq = pitch.get_value_in_frame(i + 1)
 
-        if freq is None or freq < 80 or freq > 400:
-            pitch_values.append({"time": time, "frequency": None})
-            continue
+            if freq is None or freq < 80 or freq > 400:
+                pitch_values.append({"time": time, "frequency": None})
+                continue
 
-        if not np.isnan(freq) and freq > 0:
-            log_pitch = np.log2(freq)
-            pitch_values.append({"time": time, "frequency": log_pitch})
-            if log_pitch < min_freq:
-                min_freq = log_pitch
-            if log_pitch > max_freq:
-                max_freq = log_pitch
-        else:
-            pitch_values.append({"time": time, "frequency": None})
-
-    pitch_values = clean_audio(pitch_values)
-
-    if min_freq == float('inf'):
-        raise ValueError("Unable to detect pitch from audio. Try recording again.")
-
-    norm_pitch_values = []
-    for pitch in pitch_values:
-        freq = pitch['frequency']
-        if freq is not None:
-            if max_freq == min_freq:
-                freq = 0.5
+            if not np.isnan(freq) and freq > 0:
+                log_pitch = np.log2(freq)
+                pitch_values.append({"time": time, "frequency": log_pitch})
+                if log_pitch < min_freq:
+                    min_freq = log_pitch
+                if log_pitch > max_freq:
+                    max_freq = log_pitch
             else:
-                freq = (pitch['frequency'] - min_freq) / (max_freq - min_freq)
-        norm_pitch_values.append({"time": pitch['time'], 'frequency': freq})
+                pitch_values.append({"time": time, "frequency": None})
 
-    os.remove(input_path)
-    os.remove(output_path)
+        pitch_values = clean_audio(pitch_values)
+
+        if min_freq == float('inf'):
+            raise ValueError("Unable to detect pitch from audio. Try recording again.")
+
+        norm_pitch_values = []
+        for pitch in pitch_values:
+            freq = pitch['frequency']
+            if freq is not None:
+                if max_freq == min_freq:
+                    freq = 0.5
+                else:
+                    freq = (pitch['frequency'] - min_freq) / (max_freq - min_freq)
+            norm_pitch_values.append({"time": pitch['time'], 'frequency': freq})
+
+    finally:
+        if input_path and os.path.exists(input_path):
+            os.remove(input_path)
+        if output_path and os.path.exists(output_path):
+            os.remove(output_path)
 
     return {"pitch": norm_pitch_values}
